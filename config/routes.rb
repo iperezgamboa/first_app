edfd6931066adb080eb8f754f23439831ed99b7d
Ignacio Pèrez Gamboa <i.perezgamboa@gmail.com>
2020-09-16 22:39:20 -0300

    tercercommit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/two'
-  root to: 'pages#one'
+  get 'pages/three'
+  root to: 'pages#one'  #solo para una pagina. pag de inicio.
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
