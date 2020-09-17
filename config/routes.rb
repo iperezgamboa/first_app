537b3ce4dd0d08fa115bb800d93325cadb352718
Ignacio Pèrez Gamboa <i.perezgamboa@gmail.com>
2020-09-16 22:26:22 -0300

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/two'
+  root to: 'pages#one'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
